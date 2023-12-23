36e67dafa8837e1c8451700b9b1c3c45c8fe47d1
khushi0818 <86145565+khushi0818@users.noreply.github.com>
2023-12-23 18:56:16 +0530

    node js task

---
+++ index.js
@@ -0,0 +1,10 @@
+// All task should perform by this folder.
+
+// First Creating a directory.
+const fs = require('fs')
+
+// Then name of the Directory
+fs.mkdirSync("task_automation")
+
+//After this we have to run the file in terminal . open terminal and write node index.js and the run it .
+// and the new directory is there. 
