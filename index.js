237a3cf9904be64026d64289d76465dbf00a48ce
khushi0818 <86145565+khushi0818@users.noreply.github.com>
2023-12-23 18:57:06 +0530

    node js task

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-// All task should perform by this folder.
-
-// First Creating a directory.
+//  for  creating files in directory.
 const fs = require('fs')
+// file1
+fs.writeFileSync("file1.txt","Node.js is an open source server environment")
+// file2
+fs.writeFileSync("file2.txt","Node.js runs on various platforms Windows, Linux, Unix, Mac OS X, etc.")
+// file3
+fs.writeFileSync("file3.txt","Node.js uses JavaScript on the server")
 
-// Then name of the Directory
-fs.mkdirSync("task_automation")
-
-//After this we have to run the file in terminal . open terminal and write node index.js and the run it .
-// and the new directory is there. 
+// for deleting the all three file we use
+// fs.unlinkSync("file1.txt")
+// fs.unlinkSync("file2.txt")
+// fs.unlinkSync("file3.txt")
